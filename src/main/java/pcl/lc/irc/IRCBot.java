d480eb5412b2e7ee033c8cfe3e449f2b254a8c75
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-10-07 13:48:29 -0500

    Update IRCBot.java

--- src/main/java/pcl/lc/irc/IRCBot.java
+++ src/main/java/pcl/lc/irc/IRCBot.java
@@ -248,7 +248,7 @@ public class IRCBot {
 	    } else {
 	    	//test
 		    System.out.println(user.getNick() + " is NOT cached");
-		    nsRegistration = user.getUserID();
+		    nsRegistration = user.getUserId();
 		    Calendar future = Calendar.getInstance();
 		    future.add(Calendar.MINUTE,5);
 		    userCache.put(user.getUserId(), new ExpiringToken(future.getTime(),nsRegistration));
